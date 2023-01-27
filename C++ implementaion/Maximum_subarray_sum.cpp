0abbd2f956781dca2572dd7c87cdb4eb2e63d16e
Srijoy Paul <srijoykumar1507@gmail.com>
2023-01-27 21:51:25 +0530

    Array_prob

---
+++ C++ implementaion/Maximum_subarray_sum.cpp
@@ -0,0 +1,57 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+
+using namespace std;
+
+// } Driver Code Ends
+// User function template for C++
+class Solution
+{
+public:
+    vector<int> findSubarray(int a[], int n)
+    {
+        // code here
+        vector<int> res;
+        vector<int> temp;
+        int maxSum = a[0], res = a[0];
+        res.push_back(a[0]) for (int i = 1; i < n; i++)
+        {
+            if ((maxSum + a[i]) > arr[i])
+                ;
+            temp.push_back(a[i]);
+        }
+    }
+};
+
+//{ Driver Code Starts.
+
+void printAns(vector<int> &ans)
+{
+    for (auto &x : ans)
+    {
+        cout << x << " ";
+    }
+    cout << "\n";
+}
+
+int main()
+{
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int n, i;
+        cin >> n;
+        int a[n];
+        for (i = 0; i < n; i++)
+        {
+            cin >> a[i];
+        }
+        Solution ob;
+        auto ans = ob.findSubarray(a, n);
+        printAns(ans);
+    }
+    return 0;
+}
+
+// } Driver Code Ends
